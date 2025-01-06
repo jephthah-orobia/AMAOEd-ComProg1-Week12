73e26e9c3ec708879f8c70627c4f69f9c0583aae
jephthah-orobia <jephthah.orobia@gmail.com>
2025-01-05 19:11:51 -0600

    implement task

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,17 @@
 
 using namespace std;
 
-
 int main()
 {
-
-  cout << "Hello World!" << endl;
-
-  _pause();
-  return EXIT_SUCCESS;
+  // Declare an uninitialized float array of size 20
+  float floatArray[20];
+  // Declare a reference to the array
+  float(&refArray)[20] = floatArray;
+  std::cout << "Values and logical addresses of the uninitialized float array:" << std::endl;
+  for (int i = 0; i < 20; ++i)
+  {
+    std::cout << "floatArray[" << i << "]: Value = " << refArray[i] << ", Address = " << (void *)&refArray[i] << std::endl;
+    _pause();
+    return EXIT_SUCCESS;
+  }
 }
